5862557ed002e51551940b1ee283903b324e8d3b
jose tomas ruiz <j_tomas_93@hotmail.com>
2024-11-20 12:35:19 -0300

    Creating GifAppExpert

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,16 @@
+import { useState } from 'react';
+
 export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball Z']);
+
   return (
     <>
     <h1>GifExpertApp</h1>
+
+    <ol>
+      {categories.map(category => <li key={category}>{category}</li>)}
+    </ol>
     </>
   )
 }
