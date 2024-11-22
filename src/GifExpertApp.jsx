f32a0091e360a9ea50b93b5ba523ed757a8857e5
jose tomas ruiz <j_tomas_93@hotmail.com>
2024-11-22 19:43:56 -0300

    setCategories

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -4,15 +4,21 @@ import { AddCategory } from './components/AddCategory';
 export const GifExpertApp = () => {
 
   const [categories, setCategories] = useState(['One Punch', 'Dragon Ball Z']);
-  const onAddCategory = () => {
-    setCategories([...categories, "Valorant"])
+  const onAddCategory = (newCategory) => {
+
+    if(categories.includes(newCategory)) return;
+
+    setCategories([newCategory, ...categories])
   }
 
 
   return (
     <>
     <h1>GifExpertApp</h1>
-    <AddCategory setCategories={ setCategories }/>
+    <AddCategory
+    //setCategories={ setCategories }
+      onNewCategory={onAddCategory}
+    />
 
     <ol>
       {categories.map(category => <li key={category}>{category}</li>)}
