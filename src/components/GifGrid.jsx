7f759bdaa986cdcc26fab4df91a341ab77043091
jose tomas ruiz <j_tomas_93@hotmail.com>
2024-11-24 19:32:39 -0300

    Creacion de useEffect en archivo GifGrid.jsx

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,15 +1,24 @@
+import { useState, useEffect } from "react";
 import { getGifs } from "../helpers/getGifts";
 
 
 export const GifGrid = ({category}) => {
 
 
-  getGifs(category);
+  const [counter, setCounter] = useState(10);
+
+  useEffect( () => {
+    getGifs(category);
+
+
+  }, [])
+
 
   return (
     <>
     <h3>{category}</h3>
-    <p>Hola Mundo</p>
+    <h5>{ counter }</h5>
+    <button onClick={ () => setCounter( counter +1 ) }>+1</button>
     </>
   )
 }
