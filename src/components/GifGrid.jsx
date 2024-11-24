25a744815ba83cee15cdf285067d34cc310123f9
jose tomas ruiz <j_tomas_93@hotmail.com>
2024-11-24 20:24:56 -0300

    Creacion de elementos ol y li

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,24 +1,32 @@
-import { useState, useEffect } from "react";
+import {useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifts";
 
 
 export const GifGrid = ({category}) => {
 
 
-  const [counter, setCounter] = useState(10);
+    const [images, setImages] = useState([]);
+    const getImages = async() => {
+      const newImages = await getGifs(category);
+      setImages(newImages);
+    }
+
 
   useEffect( () => {
-    getGifs(category);
+    getImages();
 
 
   }, [])
 
-
   return (
     <>
     <h3>{category}</h3>
-    <h5>{ counter }</h5>
-    <button onClick={ () => setCounter( counter +1 ) }>+1</button>
+    <ol>
+      {images.map(({ id, title}) => (
+        <li key={id}>{title}</li>
+      ))
+      }
+    </ol>
     </>
   )
 }
