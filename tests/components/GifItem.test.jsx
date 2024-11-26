28edf35d51aeeeec864266943ea8a8fbeb7b213e
jose tomas ruiz <j_tomas_93@hotmail.com>
2024-11-26 12:31:29 -0300

    Creacion de archivos de testing

---
+++ tests/components/GifItem.test.jsx
@@ -0,0 +1,18 @@
+import { render } from "@testing-library/react"
+import { GifItem } from "../../src/components/GifItem"
+
+
+describe('Pruebas en el componente <GifItem />', () => {
+
+  const title ="Saitama";
+  const url = "https://one-punch.com/saitama.jpg"
+
+  test('debe de hacer match con el snapshot', () => {
+
+    const {container} = render(<GifItem title={title} url={url}/>)
+    expect(container).toMatchSnapshot();
+
+   })
+
+
+})
